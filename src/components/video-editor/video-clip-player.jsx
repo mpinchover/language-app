74b2745b4e2c0f6550cd2a1eeb72106f6890dbde
mpinchover <mpinchover@gmail.com>
2024-11-25 23:20:10 -0800

    more progress on video player

---
+++ src/components/video-editor/video-clip-player.jsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState, useEffect, useContext } from "react";
+
+import {
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Box,
+  Button,
+} from "@chakra-ui/react";
+import { MdDelete } from "react-icons/md";
+function VideoClipPlayer({ src, startTime, endTime, idx }) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    video.muted = true;
+
+    const handleTimeUpdate = (e) => {
+      if (video.currentTime >= endTime && !video.paused) {
+        video.currentTime = startTime;
+      }
+    };
+
+    const handleLoadedMetadata = () => {
+      video.currentTime = startTime;
+    };
+
+    video.addEventListener("timeupdate", handleTimeUpdate);
+    video.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+    return () => {
+      video.removeEventListener("timeupdate", handleTimeUpdate);
+      video.removeEventListener("loadedmetadata", handleLoadedMetadata);
+    };
+  }, []);
+
+  return (
+    <Box
+      display="flex"
+      position={"relative"}
+      width="100px"
+      //   height="100px"
+      backgroundColor="black"
+      overflow="hidden"
+      aspectRatio={1}
+    >
+      <video
+        muted={true}
+        ref={videoRef}
+        autoPlay={true}
+        src={src}
+        loop={true}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+      />
+      <Button opacity={0.8} size="xs" position="absolute" top="5px" right="5px">
+        <MdDelete />
+      </Button>
+    </Box>
+  );
+}
+
+export default VideoClipPlayer;
--- src/components/video-editor/video-editor.jsx
+++ src/components/video-editor/video-editor.jsx
@@ -7,30 +7,85 @@ import {
   RangeSlider,
   RangeSliderFilledTrack,
   Button,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
 } from "@chakra-ui/react";
 import { useRef, useState, useEffect } from "react";
 import { RxScissors } from "react-icons/rx";
+import VideoClipPlayer from "./video-clip-player";
+import { FaPause, FaPlay } from "react-icons/fa";
+import { useTheme } from "@chakra-ui/react";
 
-const CutClips = () => {
-  return <Box></Box>;
+const VideoClipsList = ({ videoClips }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      alignItems="flex-start"
+      justifyContent={"flex-start"}
+      //   border="solid 19px green"
+      width="100%"
+    >
+      {videoClips.map((e, idx) => {
+        const { src, startTime, endTime } = e;
+        return (
+          <VideoClipPlayer
+            src={src}
+            startTime={startTime}
+            endTime={endTime}
+            idx={idx}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+const PlayHead = () => {
+  return (
+    <Box w="10px" h="20px">
+      AA
+    </Box>
+  );
 };
 
 const STEP = 0.25;
 const videoURL =
   "https://storage.googleapis.com/video-editor-uploads/demo_1.mp4";
 
-const VideoPlayer = () => {
+const VideoPlayer = ({ saveVideoCut }) => {
   const videoRef = useRef(null);
   const sliderRef = useRef(null); // Ref for the input slider
   const [duration, setDuration] = useState(0);
-  const [range, setRange] = useState([0, 19]);
+  const [range, setRange] = useState([0, 0]);
   const [overlays, setOverlays] = useState([]);
   const [isDragging, setIsDragging] = useState(false);
   const [startPos, setStartPos] = useState({ x: 0, y: 0 });
   const [newOverlay, setNewOverlay] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const [videoClips, setVideoClips] = useState([]);
   const [displayRange, setDisplayRange] = useState(false);
+  const theme = useTheme();
+  const blue500 = theme.colors.blue[600]; // Get the value of "blue.500"
+
+  useEffect(() => {}, []);
+  const handleCut = () => {
+    const startTime = range[0];
+    let endTime = range[1];
+    if (endTime > videoRef?.current.duration) {
+      endTime = videoRef?.current.duration;
+    }
+    saveVideoCut({
+      src: videoURL,
+      startTime,
+      endTime,
+    });
+    setRange([1, videoRef.current.duration]);
+  };
 
   useEffect(() => {
     const video = videoRef.current;
@@ -44,7 +99,9 @@ const VideoPlayer = () => {
     };
 
     const handleLoadedMetadata = () => {
+      const vidDur = Math.ceil(video.duration);
       setDuration(Math.floor(video.duration));
+      setRange([0, vidDur]);
       video.currentTime = range[0];
     };
 
@@ -92,20 +149,11 @@ const VideoPlayer = () => {
   const handleClickPlay = () => {
     if (videoRef.current && videoRef.current.paused) {
       videoRef.current.play();
+      setIsPlaying(true);
       return;
     }
     videoRef.current.pause();
-  };
-
-  const handleCut = () => {
-    const startTime = range[0];
-    const endTime = range[1];
-    // saveVideoCut({
-    //   src: videoURL,
-    //   startTime,
-    //   endTime,
-    // });
-    // setRange(1, videoRef.current.duration);
+    setIsPlaying(false);
   };
 
   const onMouseEnterVideo = () => {
@@ -115,6 +163,12 @@ const VideoPlayer = () => {
   const onMouseLeaveVideo = () => {
     setDisplayRange(false);
   };
+
+  const handleTrackClick = (e) => {
+    e.stopPropagation(); // Prevent the default click behavior
+    e.preventDefault();
+  };
+
   return (
     <Box
       onMouseEnter={onMouseEnterVideo}
@@ -125,8 +179,7 @@ const VideoPlayer = () => {
       justifyContent="center"
       alignItems="center"
       flex={1}
-      width="1000px"
-      maxW="1000px" // Limits the parent Box to 1000px
+      width="100%"
     >
       <video
         ref={videoRef}
@@ -134,16 +187,18 @@ const VideoPlayer = () => {
         loop
         muted
         style={{
-          width: "100%",
+          height: "100%",
 
           objectFit: "contain",
         }}
       />
 
       <Box
+        display="flex"
         backgroundColor="rgba(255, 255, 255, 0.6)"
         p={6}
         position="absolute"
+        flexDirection="row"
         // width="100%"
         bottom="50px"
         left="50px"
@@ -155,20 +210,41 @@ const VideoPlayer = () => {
           pointerEvents: displayRange ? "auto" : "none", // Disable interaction when hidden
         }}
       >
+        <Box
+          marginRight={6}
+          //   width="100px"
+          //   width="100%"
+          display="flex"
+          justifyContent={"center"}
+          alignItems="center"
+        >
+          {!isPlaying && (
+            <FaPlay
+              cursor={"pointer"}
+              onClick={handleClickPlay}
+              color={blue500}
+            />
+          )}
+          {isPlaying && (
+            <FaPause
+              cursor={"pointer"}
+              onClick={handleClickPlay}
+              color={blue500}
+            />
+          )}
+        </Box>
         <RangeSlider
-          // on
-          // onDragStart={onChange}
+          //   isDisabled={true}
           onChangeStart={onChangeStart}
-          // onChangeStart={onChangeStart}
           onChange={onChange}
           onChangeEnd={onChangeEnd}
           aria-label={["min", "max"]}
-          // defaultValue={range}
-
           // max={18}
           // min={1}
           step={STEP}
           value={range}
+          //   onClick={handleTrackClick}
+          // onMouseDown={}
 
           // minStepsBetweenThumbs={3}
         >
@@ -189,7 +265,7 @@ const VideoPlayer = () => {
         right="50px"
         top="50px"
       >
-        <Button>
+        <Button onClick={handleCut}>
           <RxScissors />
         </Button>
       </Box>
@@ -198,11 +274,96 @@ const VideoPlayer = () => {
 };
 
 const VideoEditor = () => {
+  const [videoClips, setVideoClips] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const saveVideoCut = (clip) => {
+    clip.idx = videoClips.length;
+    setVideoClips([...videoClips, clip]);
+  };
+
   return (
-    <Box flex={1} display="flex" justifyContent={"center"} alignItemms="center">
-      <VideoPlayer />
+    <Box
+      flexDirection={"column"}
+      flex={1}
+      display="flex"
+      justifyContent={"center"}
+      alignItems="center"
+    >
+      <VideoPlayer saveVideoCut={saveVideoCut} />
+      {videoClips.length > 0 && <VideoClipsList videoClips={videoClips} />}
     </Box>
   );
 };
 
-export default VideoEditor;
+const VideoDashboard = () => {
+  //   const [videoClips, setVideoClips] = useState([]);
+
+  //   useEffect(() => {}, []);
+  //   const handleCut = () => {
+  //     const startTime = range[0];
+  //     let endTime = range[1];
+  //     if (endTime > videoRef?.current.duration) {
+  //       endTime = videoRef?.current.duration;
+  //     }
+  //     saveVideoCut({
+  //       src: videoURL,
+  //       startTime,
+  //       endTime,
+  //     });
+  //     setRange([1, videoRef.current.duration]);
+  //   };
+
+  return (
+    <Box flex={1} display="flex" justifyContent={"center"}>
+      <Tabs
+        isLazy
+        display="flex"
+        flexDirection="column"
+        flex={1}
+        width="100%"
+        maxW="1000px"
+        variant="unstyled"
+      >
+        <TabList>
+          <Tab
+            _selected={{
+              fontWeight: "bold",
+              borderBottom: "none", // Remove any underline if it persists
+            }}
+          >
+            Cut
+          </Tab>
+          <Tab
+            _selected={{
+              fontWeight: "bold",
+              borderBottom: "none", // Remove any underline if it persists
+            }}
+          >
+            Edit
+          </Tab>
+          <Tab
+            _selected={{
+              fontWeight: "bold",
+              borderBottom: "none", // Remove any underline if it persists
+            }}
+          >
+            Preview
+          </Tab>
+        </TabList>
+
+        <TabPanels display="flex" flex={1}>
+          <TabPanel display="flex" p={0} flex={1}>
+            <VideoEditor />
+          </TabPanel>
+          {/* <TabPanel display="flex" flex={1}>
+            <PreviewVideo />
+          </TabPanel> */}
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
+};
+
+export default VideoDashboard;
